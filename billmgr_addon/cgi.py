8d45a93d9f229f8ad9b717e7992b411025a86c3b
dmitriy <dmitriydmgora@gmail.com>
2025-07-05 00:25:25 +0300

    27

--- billmgr_addon/cgi.py
+++ billmgr_addon/cgi.py
@@ -12,48 +12,49 @@ from pathlib import Path
 
 from billmgr_addon.utils.logging import LOGGER
 
-
 def run_with_cgi(application):
     environ = dict(os.environ.items())
-    environ["wsgi.input"] = sys.stdin.buffer
-    environ["wsgi.errors"] = sys.stderr
-    environ["wsgi.version"] = (1, 0)
-    environ["wsgi.multithread"] = False
-    environ["wsgi.multiprocess"] = True
-    environ["wsgi.run_once"] = True
+    environ['wsgi.input']        = sys.stdin.buffer
+    environ['wsgi.errors']       = sys.stderr
+    environ['wsgi.version']      = (1, 0)
+    environ['wsgi.multithread']  = False
+    environ['wsgi.multiprocess'] = True
+    environ['wsgi.run_once']     = True
 
-    if environ.get("HTTPS", "off") in ("on", "1"):
-        environ["wsgi.url_scheme"] = "https"
+    if environ.get('HTTPS', 'off') in ('on', '1'):
+        environ['wsgi.url_scheme'] = 'https'
     else:
-        environ["wsgi.url_scheme"] = "http"
+        environ['wsgi.url_scheme'] = 'http'
 
     headers_set = []
     headers_sent = []
 
-    is_plugin_request = environ.get("EVENT_TYPE") in ["action", "before", "after"]
+    is_plugin_request = environ.get('EVENT_TYPE') in ['action', 'before', 'after']
 
     def write(data):
+        response_str = ''
         if not headers_set:
             raise AssertionError("write() before start_response()")
 
         if not is_plugin_request and not headers_sent:
             # Before the first output, send the stored headers
             status, response_headers = headers_sent[:] = headers_set
-            sys.stdout.write(f"HTTP/1.1 {status}\r\n")
+            sys.stdout.write(f'HTTP/1.1 {status}\r\n')
             for header in response_headers:
-                sys.stdout.write("{}: {}\r\n".format(header[0], header[1]))
-            sys.stdout.write("\r\n")
+                sys.stdout.write('{}: {}\r\n'.format(header[0], header[1]))
+            sys.stdout.write('\r\n')
 
-        response_body = data.decode("utf-8")
+        response_body = data.decode('utf-8')
         sys.stdout.write(response_body)
 
     def start_response(status, response_headers, exception=None):
         if exception:
             try:
                 if headers_sent:
+                    # Re-raise original exception if headers sent
                     raise exception
             finally:
-                exception = None
+                exception = None     # avoid dangling circular ref
         elif headers_set:
             raise AssertionError("Headers already set!")
 
@@ -64,25 +65,24 @@ def run_with_cgi(application):
         result = application(environ, start_response)
     except Exception as e:
         LOGGER.exception(e)
-
         headers_set = []
         headers_sent = []
-        start_response("500 Internal Server Error", [("Content-Type", "text/plain")])
-        write("Internal Server Error".encode("utf-8"))
-        sys.exit(1)
-
+        start_response(500, [])
+        write('Something went wrong'.encode('utf-8'))
+        quit(0)
+        
+    # result = application(environ, start_response)
     try:
         for data in result:
-            if data:
+            if data:    # don't send headers until body appears
                 write(data)
         if not headers_sent:
-            write(b"")
+            write('')   # send headers now if body was empty
     finally:
-        if hasattr(result, "close"):
+        if hasattr(result, 'close'):
             result.close()
-
-    sys.exit(0)
-
+            
+    quit(0)
 
 def create_cgi_app_from_module(module_name):
     try:
--- billmgr_addon/core/__init__.py
+++ billmgr_addon/core/__init__.py
@@ -11,10 +11,18 @@
 """
 
 from types import SimpleNamespace
+import tomlkit
+from Crypto.PublicKey import RSA
 
 from flask import Flask, appcontext_pushed, g
+from flask_login import LoginManager
 
-from billmgr_addon.utils.logging import LOGGER
+from billmgr_addon.auth.auth import load_billmgr_user
+from billmgr_addon.core.error_handlers import register_error_handlers
+from billmgr_addon.db.db import DBConfig, FlaskDbExtension
+from billmgr_addon.utils.files import config_path, public_path
+from billmgr_addon.utils.logging import LOGGER, setup_logger
+from billmgr_addon.utils.serialization import CustomJSONEncoder
 
 
 def get_router():
@@ -74,7 +82,7 @@ class MgrAddonExtension:
         )
 
 
-def create_app():
+def create_common_app():
     """
     Создать базовое Flask приложение для плагина
 
@@ -83,15 +91,55 @@ def create_app():
     """
     app = Flask(__name__)
 
-    app.config.update(
-        {
-            "DEBUG": False,
-            "SECRET_KEY": "change-me-in-production",
-        }
-    )
+    app.config.from_file(config_path, load=tomlkit.load)
+
+    is_debugging = app.config.get('DEBUG', False)
+
+    app.json_encoder = CustomJSONEncoder
+    app.mgr_encryption_key = RSA.importKey(open('/usr/local/mgr5/etc/billmgr.pem').read())
+
+    billmgr_db_config = DBConfig.from_panel_name('billmgr')
+
+    FlaskDbExtension().init_app(app)
+    FlaskDbExtension() \
+        .init_app(app, billmgr_db_config, alias='billmgr')
+
+    # FIXME - make simpler Celery setup
+    # celery_app_name = 'cloud_addon'
+    # CeleryExtension().init_app(app, task_prefix=celery_app_name, include=[
+    #     'app.celery.cloud.servers',
+    #     'app.celery.cloud.ssh_keys',
+    #     'app.celery.cloud.volumes',
+    #     'app.celery.cloud.ips',
+    #     # 'app.celery.cloud.loadbalancers',
+    #     'app.celery.cloud.vrouters',
+    #     # 'app.celery.cloud.db_clusters',
+    #     'app.celery.cloud.project',
+    # ], broker_use_ssl={
+    #     'keyfile': cwd_path.joinpath(app.config['CELERY_BROKER_KEYFILE']),
+    #     'certfile': cwd_path.joinpath(app.config['CELERY_BROKER_CERTFILE']),
+    #     'ca_certs': cwd_path.joinpath(app.config['CELERY_BROKER_CA_CERTFILE']),
+    #     'cert_reqs': ssl.CERT_REQUIRED
+    # })
+
 
     return app
 
+def create_app(endpoints) -> Flask:
+    app = create_common_app()
+    app.static_folder = public_path
+
+    MgrAddonExtension().init_app(app, endpoints)
+    register_error_handlers(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.request_loader(load_billmgr_user)
+
+    # from app.blueprints.main import bp as main_bp
+    # app.register_blueprint(main_bp)
+
+    return app
 
 def create_cgi_app(endpoints):
     """
@@ -103,10 +151,15 @@ def create_cgi_app(endpoints):
     Returns:
         Flask: Настроенное CGI приложение
     """
-    app = create_app()
+    app = create_common_app()
+    app.static_folder = public_path
 
-    mgr_addon = MgrAddonExtension()
-    mgr_addon.init_app(app, endpoints)
+    MgrAddonExtension().init_app(app, endpoints)
+    register_error_handlers(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.request_loader(load_billmgr_user)
 
     return app
 
@@ -118,7 +171,7 @@ def create_cli_app():
     Returns:
         Flask: Настроенное CLI приложение
     """
-    app = create_app()
+    app = create_common_app()
     return app
 
 
